8c24214ae7335ecfd0fb0aa416e1303dc7deae15
Nisse <nielsen.rasmus@pm.me>
2020-09-16 13:07:26 +0200

    Fix funcs that need more than one arg to go through parse func

--- nisserino/cmd_loop.py
+++ nisserino/cmd_loop.py
@@ -5,25 +5,23 @@ daycare = Dog_daycare("Vacker Tass", "Nisse")
 
 
 class Daycare_loop(cmd.Cmd):
-    into = "Welcome to the daycare"
+    intro = "Welcome to the daycare"
     prompt = "(Daycare): "
     file = None
 
     def do_show_dogs(self, arg):
         'Show the names of all dogs in the Day-care!'
         try:
-            daycare.all_dogs()
+            daycare.show_dogs()
         except Exception as e:
             print(f"Error: {e}")
 
-    def do_add_dog(self, arg):  # Fix strip in some sense
+    def do_add_dog(self, arg):
         'Add a dog: add_dog name,age,owner'
-        daycare.add_dog(*parse(arg))
-        # try:
-        #     name, age, owner = arg.split(",")
-        #     daycare.add_dog(name.lower().strip(), int(age), owner.lower().strip())
-        # except Exception as e:
-        #     print(f"Error: {e}")
+        try:
+            daycare.add_dog(*parse(arg))
+        except Exception as e:
+            print(f"Error: {e}")
 
     def do_remove_dog(self, dog):  # Add option to remove by id/owner/age
         'Remove a dog: remove_dog dog_name'
@@ -49,48 +47,48 @@ class Daycare_loop(cmd.Cmd):
     def do_dog_name(self, arg):
         'Change dog name: dog_name name, newname'
         try:
-            dog, new_name = arg.split(",")
-            daycare.find_dog(dog.lower().strip()).set_name(new_name.lower().strip())
+            dog, new_name = parse(arg)
+            daycare.find_dog(dog).set_name(new_name)
         except Exception as e:
             print(f"Error: {e}")
 
     def do_dog_owner(self, arg):
         'Change dog owner: dog_owner dog_name, new_owner'
         try:
-            dog, new_owner = arg.split(",")
-            daycare.find_dog(dog.lower().strip()).set_owner(new_owner.lower().strip())
+            dog, new_owner = parse(arg)
+            daycare.find_dog(dog).set_owner(new_owner)
         except Exception as e:
             print(f"Error: {e}")
 
     def do_dog_toy(self, arg):
         'Change dog toy: dog_name, toy'
         try:
-            dog, toy = arg.split(",")
-            daycare.find_dog(dog.lower().strip()).set_toy(toy.lower().strip())
+            dog, toy = parse(arg)
+            daycare.find_dog(dog).set_toy(toy)
         except Exception as e:
             print(f"Error: {e}")
 
     def do_dog_age(self, arg):
         'Change dog age: dog_name, age'
         try:
-            dog, new_age = arg.split(",")
-            daycare.find_dog(dog.lower().strip()).set_age(int(new_age))
+            dog, new_age = parse(arg)
+            daycare.find_dog(dog).set_age(new_age)
         except Exception as e:
             print(f"Error: {e}")
 
     def do_dog_breed(self, arg):
         'Change dog breed: dog_name, dog_breed'
         try:
-            dog, breed = arg.split(",")
-            daycare.find_dog(dog.lower().strip()).set_breed(breed.lower().strip())
+            dog, breed = parse(arg)
+            daycare.find_dog(dog).set_breed(breed)
         except Exception as e:
             print(f"Error: {e}")
 
     def do_dog_bff(self, arg):
         'Add dogs bff: dog_bff dog_name, bffs_name'
         try:
-            dog, bff = arg.split(",")
-            daycare.find_dog(dog.lower().strip()).add_bff(daycare.find_dog(bff.lower().strip()))
+            dog, bff = parse(arg)
+            daycare.find_dog(dog).add_bff(daycare.find_dog(bff))
         except Exception as e:
             print(f"Error: {e}")
 
